cdaa3985badad7393512055903d2277e418626eb
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2023-05-27 15:48:10 +0200

    fix inheritance

--- cpp-engine/pieces.h
+++ cpp-engine/pieces.h
@@ -7,49 +7,50 @@ namespace Pieces{
 			int pieceValue;
 			int pieceType;
 	};
-	class Pawn : Piece{
+	class Pawn : public Piece{
 		public:
 			Pawn(int color);
 			const static int pieceType = 0;
 			const static int pieceValue = 1;
 			int color;
 	};
-	class Bishop : Piece{
+	class Bishop : public Piece{
 		public:
 			Bishop(int color);
 			const static int pieceType = 2;
 			int color;
 			const static int pieceValue = 3.1;
 	}; 
-	class Knight : Piece{
+	class Knight : public Piece{
 		public:
 			Knight(int color);
 			const static int pieceType = 1;
 			int color;
 			const static int pieceValue = 3;
 	}; 
-	class Rook : Piece{
+	class Rook : public Piece{
 		public:
 			Rook(int color);
 			const static int pieceType = 3;
 			int color;
 			const static int pieceValue = 5;
 	};
-	class Queen : Piece{
+	class Queen : public Piece{
 		public:
 			Queen(int color);
 			const static int pieceType = 4;
 			int color;
 			const static int pieceValue = 9;
 	};
-	class King : Piece{
+	class King : public Piece{
 		public:
 			King(int color);
 			const static int pieceType = 5;
 			int color;
 			const static int pieceValue = 0;
 	};
-	class EmptySquare{
+	class EmptySquare : public Piece{
+		public:
 		EmptySquare();
 		const static int pieceType = -1;
 		const static int color = -1;
